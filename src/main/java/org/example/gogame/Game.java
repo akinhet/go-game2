ce17cd951d9fd096a83cc553465e9ec5eef9e9d0
Jan-Langier <283955@student.pwr.edu.pl>
2025-12-12 12:10:17 +0100

    Game class add, many more things to do in it, todo - implement public Game(); check if everything is good after adding other classes

---
+++ src/main/java/org/example/gogame/Game.java
@@ -0,0 +1,45 @@
+package org.example.gogame;
+
+import java.util.ArrayList;
+
+public class Game {
+    //todo - implement
+    private PlayerHandler blackPlayer;
+    private PlayerHandler whitePlayer;
+    private PlayerHandler currentPlayer = blackPlayer;
+    private int size;
+    private Board board = new Board(size);
+    private GameLogic gameLogic;
+    private ArrayList<int[]> captures = new ArrayList<>();
+    private StoneColor finalform;
+    //todo - implement
+    public Game(PlayerHandler p1, PlayerHandler p2, int size) {}
+
+    public synchronized void processMove(int x, int y, PlayerHandler player){
+        String msg = "";
+        if (gameLogic.validateMove(board,x,y,player.myColor)){
+            gameLogic.checkGroup(board,x,y,player.myColor);
+            captures = gameLogic.checkCaptures(board,x,y,player.myColor);
+            finalform = gameLogic.finalCheck(board,x,y,player.myColor);
+            msg += x + " ";
+            msg += y + " ";
+            msg += finalform.name() + ";";
+            for (int[] point : captures){
+                msg += point[0] + " ";
+                msg += point[1] + " ";
+                msg += "EMPTY";
+            }
+            BroadcastMessage(msg,blackPlayer);
+            BroadcastMessage(msg,whitePlayer);
+        }else {
+            BroadcastMessage("Bad move - put valid move",player);
+        }
+
+    }
+    private void switchTurn(){
+        currentPlayer = (currentPlayer == blackPlayer ? whitePlayer : whitePlayer);
+    }
+    private void BroadcastMessage(String message, PlayerHandler player){
+        player.sendMessage(message);
+    }
+}
