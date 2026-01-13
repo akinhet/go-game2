f5f5d4cb3c55a415892a38567b3c0c3af5ab7832
Jan-Langier <283955@student.pwr.edu.pl>
2026-01-13 21:58:54 +0100

    Tests, UML

--- src/test/java/org/example/gogame/server/GameLogicTest.java
+++ src/test/java/org/example/gogame/server/GameLogicTest.java
@@ -57,4 +57,53 @@ class GameLogicTest {
 
         assertTrue(captures.isEmpty(), "Nie powinno być zbicia, kamień ma oddech");
     }
+
+    @Test
+    void testKoRule(){
+        board.setStone(1,0, StoneColor.BLACK);
+        board.setStone(0,1, StoneColor.BLACK);
+        board.setStone(2,1, StoneColor.BLACK);
+        board.setStone(1,2, StoneColor.BLACK);
+        board.setStone(1,3, StoneColor.WHITE);
+        board.setStone(0,2, StoneColor.WHITE);
+        board.setStone(2,2, StoneColor.WHITE);
+        board.setStone(1,1, StoneColor.WHITE);
+        ArrayList<int[]> captures = gameLogic.checkCaptures(board,1,1,StoneColor.WHITE);
+        board.setStone(captures.getFirst()[0],captures.getFirst()[1],StoneColor.EMPTY);
+        int[] lastMove = new int[]{captures.getFirst()[0],captures.getFirst()[1]};
+        assertTrue(gameLogic.isKo(board,lastMove,captures.getFirst()[0],captures.getFirst()[1]));
+    }
+
+    @Test
+    void testSuicide(){
+        board.setStone(1,0, StoneColor.BLACK);
+        board.setStone(0,1, StoneColor.BLACK);
+        board.setStone(2,1, StoneColor.BLACK);
+        board.setStone(1,2, StoneColor.BLACK);
+        board.setStone(1,1, StoneColor.WHITE);
+        StoneColor suicide = gameLogic.finalCheck(board,1,1,StoneColor.WHITE);
+        assertEquals(StoneColor.EMPTY,suicide);
+    }
+
+    @Test
+    void testTerritoryCounting() {
+        //Otoczone czarnymi
+        board.setStone(1, 0, StoneColor.BLACK);
+        board.setStone(0, 1, StoneColor.BLACK);
+
+        //Otoczone białymi
+        board.setStone(4, 5, StoneColor.WHITE);
+        board.setStone(6, 5, StoneColor.WHITE);
+        board.setStone(5, 4, StoneColor.WHITE);
+        board.setStone(5, 6, StoneColor.WHITE);
+
+        //Otoczone różnymi
+        board.setStone(7, 8, StoneColor.BLACK);
+        board.setStone(8, 7, StoneColor.WHITE);
+
+        int[] scores = gameLogic.countTerritory(board);
+
+        assertEquals(1, scores[0], "Czarne terytorium powinno wynosić 1");
+        assertEquals(1, scores[1], "Białe terytorium powinno wynosić 1");
+    }
 }
